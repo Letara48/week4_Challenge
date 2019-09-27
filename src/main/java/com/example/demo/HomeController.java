251e883c3e332f871fdcf8f2ea641985047f4247
letara48 <lgirl96@hotmail.com>
2019-09-27 15:15:45 -0400

    first commmit:

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -17,6 +17,38 @@ public class HomeController {
     public String other(){
         return "blog2";
     }
+    @RequestMapping("/blog3")
+    public String fish4(){
+        return "blog3";
+    }
+    @RequestMapping("/blog4")
+    public String fish5(){
+        return "blog4";
+    }
+    @RequestMapping("/blog5")
+    public String fish6(){
+        return "blog5";
+    }
+    @RequestMapping("/blog6")
+    public String fish7(){
+        return "blog6";
+    }
+    @RequestMapping("/blog7")
+    public String fish8(){
+        return "blog7";
+    }
+    @RequestMapping("/blog8")
+    public String fish9(){
+        return "blog8";
+    }
+    @RequestMapping("/blog10")
+    public String fish11(){
+        return "blog10";
+    }
+    @RequestMapping("/blog9")
+    public String fish10(){
+        return "blog9";
+    }
     @RequestMapping("/about")
     public String dessert(){
         return "about";
